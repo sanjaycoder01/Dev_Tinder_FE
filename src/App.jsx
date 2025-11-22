d0ba5eb3ada929a817491df358ab9b6873f7de5e
sanjaycoder01 <sjkulkarni31@gmail.com>
2025-11-22 17:07:23 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './Navbar'
+import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import Body from './Body'
-import Login from './Login'
-import Profile from './Profile'
+import Body from './components/Body'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import { Provider } from 'react-redux'
+import appstore from './utils/appstore'
+import Feed from './components/Feed'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Provider store={appstore}>
     <BrowserRouter basename='/'>
     <Routes>
       <Route path='/' element={<Body />} >
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Feed />} />
         <Route path='/login' element={<Login />} />
         <Route path='/profile' element={<Profile />} />
       </Route>
     </Routes>
     </BrowserRouter>
-    
+    </Provider>
     </>
   )
 }
